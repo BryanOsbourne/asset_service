9dc767bc46cfbe71cc63c316998b13e2a1ded9d0
Bryan Pacheco <97611868+BryanOsbourne@users.noreply.github.com>
2026-06-26 14:00:57 -0500

    Actualiuzacion

--- src/main/java/co/com/assets_service/dto/MaintenanceActivityResponseDTO.java
+++ src/main/java/co/com/assets_service/dto/MaintenanceActivityResponseDTO.java
@@ -4,10 +4,13 @@ import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import co.com.assets_service.enums.MaintenanceTypeActivity;
 
+import java.time.LocalDateTime;
+
 @Data
 public class MaintenanceActivityResponseDTO {
     private Long id;
     private String observation;
+    private LocalDateTime dateCreation;
     private MaintenanceTypeActivity maintenanceTypeActivity;
     @JsonProperty("maintenance_computer")
     private MaintenanceComputerResponseDTO maintenanceComputerResponseDTO;
